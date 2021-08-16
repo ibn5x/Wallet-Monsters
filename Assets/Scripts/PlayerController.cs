cf5929f1bf6124cfb964fcdc927059c519423cdb
ibn5x <87243859+ibn5x@users.noreply.github.com>
2021-08-16 13:27:01 -0400

    implemented intelligent flip for character spirite facing direction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public bool facedRight;
+    
+    public float horizontalValue;
     public float speed; 
     private Rigidbody2D rb;
     private float moveInput;
@@ -16,13 +19,28 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         moveInput = Input.GetAxis("Horizontal"); //-1  move left | 0 stand still | 1 move right
-        rb.velocity = new Vector2(moveInput*speed, rb.velocity.y);
+        rb.velocity = new Vector2(moveInput*speed, rb.velocity.y);     
 
+    }
+
+    void Move() 
+    {
+        horizontalValue = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+    }
+
+    void ProperFlip() 
+    {
+        if((horizontalValue < 0 && facedRight) || (horizontalValue > 0 && !facedRight))
+        {
+            facedRight = !facedRight;
+            transform.Rotate(new Vector3(0, 180, 0));
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        ProperFlip();
     }
 }
