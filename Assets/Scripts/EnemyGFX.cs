9821939f29c7503dbf95dab2540f2ec0046d7ab2
ibn5x <87243859+ibn5x@users.noreply.github.com>
2021-08-17 23:00:59 -0400

    implemented NPC, with ai tracking and pathfinding as well as collision detection

---
+++ Assets/Scripts/EnemyGFX.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyGFX : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
